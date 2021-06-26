c23d1c506a5d3809bc50b1dfb2bbbed9de1f8d37
yuling <yulinglin@protonmail.com>
2021-06-26 21:06:44 +0800

    refactor: Create a new component in storyComponent.js and extract the code from App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "App.css";
-import getStoryId from "./hackerNewsApi";
-//Import Components
-import Nav from "./Nav";
+import { getStoryId } from "./hackerNewsApi";
+import { getStory } from "./hackerNewsApi"
+import { StoryComponent } from "./storyComponent";
 
 const App = () => {
-  const [storyId, setStoryId] = useState([]);
-  useEffect(() => {
-    getStoryId()
-      .then(returnedData => setStoryId(returnedData))
-  }, [])
-
   return (
-    < div >
-      <div>
-        <h5>{JSON.stringify(storyId)}</h5>
-      </div>
-    </div >)
-
+    <StoryComponent />
+  )
 };
 
 export default App
--- src/hackerNewsApi.js
+++ src/hackerNewsApi.js
@@ -6,8 +6,8 @@ export const baseUrl = `https://hacker-news.firebaseio.com/v0/`
 export const newStoryId = `${baseUrl}newstories.json`
 export const storyUrl = `${baseUrl}item/`
 
-/*GET request*/
-const getStoryId = async () => {
+/*GET request for ID*/
+export const getStoryId = async () => {
     const result = await axios.get(newStoryId)
         .then(data => {
             console.log("checking data", data)
@@ -17,4 +17,15 @@ const getStoryId = async () => {
     return result
 }
 
-export default getStoryId
+/*GET request for sotry*/
+export const getStory = async (id) => {
+    const result = await axios.get(`${storyUrl + id}.json`)
+        .then(storyData => {
+            console.log("This is the story", storyData)
+            return storyData
+        })
+    console.log("This is the result in get story", result)
+    return result
+}
+
+
